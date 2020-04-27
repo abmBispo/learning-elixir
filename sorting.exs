ad6ef5cd41a6ee4e774ca596fe080f5d1f98e08c
Alan Borges <sr.alan.bispo@gmail.com>
2020-04-27 20:24:45 -0300

    Nice selection sort comparison

--- sorting.exs
+++ sorting.exs
@@ -2,17 +2,59 @@ defmodule Sort do
   def selection([]), do: []
 
   def selection(enumerable) do
-    min_value = Enum.reduce(enumerable, fn(n, callback) ->
-      if n < callback do
-        n
-      else
-        callback
-      end
-    end)
-
+    min_value = min(enumerable)
     [min_value | List.delete(enumerable, min_value) |> selection()]
+  end
+
+  def min([first | tail]) do
+    smaller(first, min(tail))
+  end
+
+  def min([]), do: []
+
+  def smaller(n1, n2) do
+    if n1 <= n2 do n1 else n2 end
   end
 end
 
-list = Sort.selection([4, 3, 5, 2, 1])
-IO.inspect list, label: "The ordered list is"
+defmodule Selection do
+  def sort(list) when is_list(list) do
+    do_selection(list, [])
+  end
+
+  def do_selection([head|[]], acc) do
+    acc ++ [head]
+  end
+  def do_selection(list, acc) do
+    min = min(list)
+    do_selection(:lists.delete(min, list), acc ++ [min])
+  end
+
+  defp min([first|[second|[]]]) do
+    smaller(first, second)
+  end
+  defp min([first|[second|tail]]) do
+    min([smaller(first, second)|tail])
+  end
+
+  defp smaller(e1, e2) do
+    if e1 <= e2 do e1 else e2 end
+  end
+end
+
+defmodule Time do
+  def now, do: ({msecs, secs, musecs} = :erlang.now; ((msecs*1000000 + secs)*1000000 + musecs)/1000000)
+end
+
+random_list = (1..10000) |> Enum.map(fn _ -> :rand.uniform(1000) end)
+
+t = Time.now
+Selection.sort(random_list)
+prepending_selection_sort = Time.now - t
+
+t = Time.now
+Sort.selection(random_list)
+appending_selection_sort = Time.now - t
+
+IO.puts "Running Elixir selection sort with prepeding takes #{prepending_selection_sort} seconds"
+IO.puts "Running Elixir selection sort with appending takes #{appending_selection_sort} seconds"
